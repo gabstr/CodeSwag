84078d9d571a0a6698b144b1f325baa12deb3531
gabstr <mnml@posteo.net>
2018-02-22 18:28:10 +0100

    AdapterView onItemClickListener

--- app/src/main/java/de/gabstr/coderswag/controller/MainActivity.kt
+++ app/src/main/java/de/gabstr/coderswag/controller/MainActivity.kt
@@ -2,26 +2,27 @@ package de.gabstr.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.widget.AdapterView
+import android.widget.Toast
 import de.gabstr.coderswag.R
 import de.gabstr.coderswag.adapters.CategoryAdapter
-import de.gabstr.coderswag.model.Category
 import de.gabstr.coderswag.services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var adapter: ArrayAdapter<Category>
     lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        adapter = ArrayAdapter(this,
-//                android.R.layout.simple_list_item_1,
-//                DataService.categories)
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter;
+
+        categoryListView.setOnItemClickListener { adapterView, view, i, l ->
+            val category = DataService.categories[i]
+            Toast.makeText(this, "You clicked on the ${category.title}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
