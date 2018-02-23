554d0575317edea9b4efa8ff2379af9cc323d76e
gabstr <mnml@posteo.net>
2018-02-23 09:11:49 +0100

    lesson 07

--- app/src/main/java/de/gabstr/coderswag/controller/ProductsActivity.kt
+++ app/src/main/java/de/gabstr/coderswag/controller/ProductsActivity.kt
@@ -1,17 +1,40 @@
 package de.gabstr.coderswag.controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import de.gabstr.coderswag.EXTRA_CATEGORY
 import de.gabstr.coderswag.R
+import de.gabstr.coderswag.adapters.ProductsAdapter
+import de.gabstr.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2// columns
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        // for large devices (Tablets...)
+        val screenSize = resources.configuration.screenWidthDp
+        if(screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
